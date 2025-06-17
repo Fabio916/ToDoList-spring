e7435ff96130a516edcbd8f3ce5f334a943e8268
Fabio <ferreirafaabio04@gmail.com>
2025-06-17 19:41:52 -0300

    Tasks update implementation

--- src/main/java/com/example/ToDoList/resources/TasksResource.java
+++ src/main/java/com/example/ToDoList/resources/TasksResource.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,11 @@ public class TasksResource {
 	public ResponseEntity<List<Tasks>> findAll(){
 		List<Tasks> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Tasks> update(@RequestBody Tasks task, @PathVariable Long id) {
+		task = service.update(task, id);
+		return ResponseEntity.ok().body(task);
 	}
 }
--- src/main/java/com/example/ToDoList/services/TasksService.java
+++ src/main/java/com/example/ToDoList/services/TasksService.java
@@ -17,4 +17,18 @@ public class TasksService {
 	public List<Tasks> findAll() {
 		return tasksRepository.findAll();
 	}
+	
+	public void delete(Long id) {
+		tasksRepository.deleteById(id);
+	}
+	
+	public Tasks update(Tasks obj, Long id) {
+		Tasks updateTask = tasksRepository.getReferenceById(id);
+		updateData(updateTask, obj);
+		return tasksRepository.save(updateTask);
+	}
+
+	private void updateData(Tasks updateTask, Tasks obj) {
+		updateTask.setDescription(obj.getDescription());
+	}
 }
